a52fcdff3a321a8fd5f31c088962243e7ea4b9e8
ashutoshmenon <ashutosh.menon@gmail.com>
2023-10-12 13:53:09 +0530

    The actual code

--- Selenium.py
+++ Selenium.py
@@ -1,1 +1,12 @@
-print("Hi")
+import selenium
+from selenium.webdriver.common.by import By
+#help(selenium)
+
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get("https://www.devicespecifications.com/en/model/e48a5dad")
+my_element = driver.find_elements(By.ID, 'model-brief-specifications')
+print(driver.title)
+for i in my_element:
+    print(i.text)
+driver.quit()
